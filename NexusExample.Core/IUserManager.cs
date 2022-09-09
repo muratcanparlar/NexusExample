0ea5f782dc0310f94e8770467135cf95ba2eea19
Murat Can PARLAR <can.parlar@invicti.com>
2022-09-09 13:34:44 +0300

    Add project files.

---
+++ NexusExample.Core/IUserManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NexusExample.Core
+{
+    public interface IUserManager
+    {
+        string Add();
+        string Delete();
+    }
+}
---
+++ NexusExample.Core/UserManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NexusExample.Core
+{
+    public class UserManager : IUserManager
+    {
+        public string Add()
+        {
+            return "User Added";
+        }
+
+        public string Delete()
+        {
+            return "User Deleted";
+        }
+    }
+}
